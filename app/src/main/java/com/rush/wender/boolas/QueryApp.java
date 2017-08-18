cb94372ecae7807c11297aee0990eaf7d8b325c3
rushwender <rushwender@gmail.com>
2017-08-18 23:10:23 +0800

    added volley request bridge for submit api

---
+++ app/src/main/java/com/rush/wender/boolas/QueryApp.java
@@ -0,0 +1,16 @@
+package com.rush.wender.boolas;
+
+/**
+ * Created by Fitri San on 8/18/2017.
+ */
+
+public class QueryApp {
+
+    public static final String DATA_URL = "http://boolas.000webhostapp.com/QueryApp.php";
+    public static final String APP_ID = "activity_id";
+    public static final String APP_NAME = "activity_name";
+    public static final String APP_FROM = "time_from";
+    public static final String APP_TO = "time_to";
+    public static final String APP_DESC = "activity_desc";
+    public static final String JSON_ARRAY = "result";
+}
